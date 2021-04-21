4ab0ca04b12e9ca0c6819556f948d66547a19ff3
Jerod Lockhart <j.r.1327@gmail.com>
2021-04-20 22:42:23 -0500

    Pushing food

---
+++ Fast Food/Assets/Scripts/Factory/HealthyManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthyManager : FoodInstance
+{
+    public void Operate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Fast Food/Assets/Scripts/Factory/JunkManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JunkManager : FoodInstance
+{
+    public void Operate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
